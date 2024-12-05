4fa248ccbbf8d89d757475d8a671bbec5641e4b2
QuinnNCH <quinn.hamelin@gmail.com>
2024-12-04 23:17:00 -0500

    refactor: simplfy code with helper functions

--- yt-dlp-py.py
+++ yt-dlp-py.py
@@ -9,6 +9,12 @@ def promptForFormat():
             message="What format do you need?",
             choices=[FileFormat.MP4, FileFormat.MP3, FileFormat.WEBM],
             )]
+def promptForImportType():
+    return [inquirer.List(
+            "type",
+            message="What type of import are you using?",
+            choices=[ImportType.BATCH, ImportType.LINK],
+            )]
     
 def batchUploadVideo(batchFilePath, format):
     subprocess.run(["./Assets/yt-dlp", "-f", format, "--ffmpeg-location", "./Assets/ffmpeg-2024-10-02-git-358fdf3083-full_build/bin/ffmpeg.exe", "-P","Output", "-a", batchFilePath])
@@ -22,6 +28,18 @@ def linkUploadVideo(youTubeLink, format):
 def linkUploadSound(youTubeLink):
     subprocess.run(["./Assets/yt-dlp", "-x","--audio-format","mp3","--ffmpeg-location", "./Assets/ffmpeg-2024-10-02-git-358fdf3083-full_build/bin/ffmpeg.exe", "-P","Output", youTubeLink])
 
+def uploadMedia(format, media, videoUploadFunc, soundUploadFunc):
+    match format:
+        case FileFormat.MP4:
+            videoUploadFunc(media, FileFormat.MP4)
+        case FileFormat.WEBM:
+            videoUploadFunc(media, FileFormat.WEBM)
+        case FileFormat.MP3:
+            soundUploadFunc(media)
+        case _:
+            print("Please select valid download option and try again")
+
+
 typeQuestion = [
     inquirer.List(
         "type",
@@ -29,38 +47,20 @@ typeQuestion = [
         choices=[ImportType.BATCH, ImportType.LINK],
     ),
 ]
-typeState = inquirer.prompt(typeQuestion)
+typeState = inquirer.prompt(promptForImportType())
 pprint(typeState)
 match typeState["type"]:
  case ImportType.BATCH:
     batchFilePath = "./Input/URLS.txt"
-    formatQuestions = promptForFormat()
-    state = inquirer.prompt(formatQuestions)
+    state = inquirer.prompt(promptForFormat())
     # TODO - are both of the below lines needed?
     pprint(state["format"])
     pprint(state["format"])
-    match state["format"]:
-        case FileFormat.MP4:
-            batchUploadVideo(batchFilePath, FileFormat.MP4)
-        case FileFormat.WEBM:
-            batchUploadVideo(batchFilePath, FileFormat.WEBM)
-        case FileFormat.MP3:
-            batchUploadSound(batchFilePath)
-        case _:
-            print("Please select valid download option and try again")
+    uploadMedia(state["format"], batchFilePath, batchUploadVideo, batchUploadSound)
  case ImportType.LINK:   
     youTubeLink = str(input("PASTE YOUR YOUTUBE LINK: "))
-    formatQuestions = promptForFormat()
-    state = inquirer.prompt(formatQuestions)
+    state = inquirer.prompt(promptForFormat())
     pprint(state["format"])
-    match state["format"]:
-        case FileFormat.MP4:
-            linkUploadVideo(youTubeLink, FileFormat.MP4)
-        case FileFormat.WEBM:
-            linkUploadVideo(youTubeLink, FileFormat.WEBM)
-        case FileFormat.MP3:
-            linkUploadSound(youTubeLink)
-        case _:
-            print("Please select valid download option and try again")
+    uploadMedia(state["format"], youTubeLink, linkUploadVideo, linkUploadSound)
 
 input("Press enter to continue.....")
